a58945455f015c0e0dca4547aa142ddc4b273e3d
Cameron <cameronhunt88@hotmail.com>
2021-10-31 09:46:08 +0000

    Initial commit

---
+++ static/JS/search.js
@@ -0,0 +1,17 @@
+function searchBar() {
+    var input, filter, ul, li, a, i, value;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("city-list");
+    li = document.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        value = a.textContent || a.innerText;
+        if (value.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
