bedf16dfd2bd088c2e9bad4d965373b2ebee6ecc
Cameron <cameronhunt88@hotmail.com>
2021-12-07 16:28:46 +0000

    removed: unnecessary code

--- init_db.py
+++ init_db.py
@@ -2,14 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('db/database.db')
 
-
 with open('db/schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
-
-#cur.execute("INSERT INTO data (title) VALUES (?)",
-#            ('Edinburgh',))
 
 connection.commit()
 connection.close()
